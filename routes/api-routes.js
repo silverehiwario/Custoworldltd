1b72a66fdae1031bf26065f44b8efacce3560a59
silverehiwario <silverehiwario@gmail.com>
2018-01-11 21:06:31 -0600

    contact page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -231,12 +231,12 @@ app.get("/index", function(req, res) {
      var nodemailer = require("nodemailer");
 
     let transporter = nodemailer.createTransport({
-        service:'Gmail',
-        //port:587,
-        //secure:false, // true for 465, false for other ports
+        host:'smtp.gmail.com',
+        port:587,
+        secure:false, // true for 465, false for other ports
         auth:{
-            user:'custoworldltd@gmail.com', // generated ethereal user
-            pass:'custoworld126'  // generated ethereal password
+            user:'silverehiwario@gmail.com', // generated ethereal user
+            pass:'michael01*'  // generated ethereal password
         },
         tls:{
           rejectUnauthorized:false
