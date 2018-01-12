c52201295b724b037dffdeb9f47770d4ec452659
silverehiwario <silverehiwario@gmail.com>
2018-01-11 21:46:43 -0600

    contact page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -235,8 +235,8 @@ app.get("/index", function(req, res) {
         port:587,
         secure:false, // true for 465, false for other ports
         auth:{
-            user:'silverehiwario@gmail.com', // generated ethereal user
-            pass:'michael01*'  // generated ethereal password
+            user:'custoworldltd@gmail.com', // generated ethereal user
+            pass:'custoworld126'  // generated ethereal password
         },
         tls:{
           rejectUnauthorized:false
